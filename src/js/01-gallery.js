d696b7d48652f4a39de6e6780b8725b839c5e1d0
mepinit <evgenmep@gmail.com>
2023-05-31 17:06:14 +0300

    5

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,11 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
 // Описаний в документації
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
 
 
 console.log(galleryItems);
